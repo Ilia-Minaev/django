071892006740cc8a46ed9aadfef061bb57eb170f
Ilia-Minaev <mi1127526540@gmail.com>
2023-05-06 00:33:45 -0300

    2023.05.06

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def index(request):
+    # print(request)
+    return HttpResponse('Hello')
+
+
+def test(request):
+    # print(request)
+    return HttpResponse('<h1>test</h1>')
