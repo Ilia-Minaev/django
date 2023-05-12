c4a3e9dba620fee7ad4aa0f9fda786b8e0e0ff98
Ilia-Minaev <mi1127526540@gmail.com>
2023-05-11 22:44:13 -0300

    2023.05.11

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,15 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import News
+from .models import News, Category
 # Create your views here.
 
 
 def index(request):
     news = News.objects.order_by('-created_at')
+
     # res = '<h1>Список новостей</h1>'
     # for item in news:
     #    res += f'<div><p>{item.title}</p></div>'
     #    res += f'<div><p>{item.content}</p></div>'
     #    res += f'<hr>'
     # return HttpResponse(res)
-    return render(request, 'news/index.html', {'news': news, 'title': 'Список новостей'})
+    context = {
+        'title': 'Список новостей',
+        'news': news,
+
+    }
+    return render(request, template_name='news/index.html', context=context)
+
+
+def get_category(request, category_id):
+    news = News.objects.filter(category_id=category_id)
+
+    category = Category.objects.get(pk=category_id)
+    context = {
+        'title': 'Список новостей',
+        'news': news,
+
+        'category': category,
+    }
+    return render(request, template_name='news/category.html', context=context)
