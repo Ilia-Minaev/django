0c4bea8e6aa9d378a3808f23ce1f349bbc081bee
Ilia-Minaev <mi1127526540@gmail.com>
2023-05-20 17:41:34 -0300

    2023.05.19

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,7 +1,8 @@
 from typing import Any, Dict
 from django.db.models.query import QuerySet
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
 
 from .models import News, Category
 from .forms import NewsForm
@@ -12,6 +13,7 @@ class HomeNews(ListView):
     template_name = 'news/home_news_list.html'
     context_object_name = 'news'
     # extra_context = {'title': 'Главная', }
+    allow_empty = True
 
     def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
         context = super().get_context_data(**kwargs)
@@ -23,6 +25,7 @@ class HomeNews(ListView):
         return super().get_queryset().filter(id_published=True)
 
 
+"""
 def index(request):
     news = News.objects.order_by('-created_at')
 
@@ -38,8 +41,26 @@ def index(request):
 
     }
     return render(request, template_name='news/index.html', context=context)
+"""
 
 
+class NewsByCategory(ListView):
+    model = News
+    template_name = 'news/home_news_list.html'
+    context_object_name = 'news'
+    allow_empty = False
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['title'] = Category.objects.get(pk=self.kwargs['category_id'])
+        return context
+
+    def get_queryset(self) -> QuerySet[Any]:
+        # return News.objects.filter(id_published=True)
+        return super().get_queryset().filter(category_id=self.kwargs['category_id'], id_published=True)
+
+
+"""
 def get_category(request, category_id):
     news = News.objects.filter(category_id=category_id)
 
@@ -50,8 +71,26 @@ def get_category(request, category_id):
         'category': category,
     }
     return render(request, template_name='news/category.html', context=context)
+"""
 
 
+class ViewNews(DetailView):
+    model = News
+    # pk_url_kwarg = 'news_id'
+    template_name = 'news/news_detail.html'
+    context_object_name = 'news_item'
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['title'] = News.objects.get(pk=self.kwargs['pk'])
+        return context
+
+    def get_queryset(self) -> QuerySet[Any]:
+        # return News.objects.filter(id_published=True)
+        return super().get_queryset().filter(id_published=True)
+
+
+"""
 def view_news(request, news_id):
     # news_item = News.objects.get(pk=news_id)
     news_item = get_object_or_404(News, pk=news_id)
@@ -60,8 +99,21 @@ def view_news(request, news_id):
         'news_item': news_item,
     }
     return render(request, template_name='news/view_news.html', context=context)
+"""
 
 
+class CreateNews(CreateView):
+    form_class = NewsForm
+    template_name = 'news/add_news.html'
+    # success_url = reverse_lazy('home')
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Добавить новость'
+        return context
+
+
+"""
 def add_news(request):
     form = ''
     if (request.method == 'POST'):
@@ -79,3 +131,4 @@ def add_news(request):
         'form': form,
     }
     return render(request, template_name='news/add_news.html', context=context)
+"""
